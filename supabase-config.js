3960e4afa5f3cc428cd16363fcd41f459d9fbd84
sakkranonkhatwong2528-lgtm <sakkranonkhatwong2528@gmail.com>
2026-08-20 19:18:03 +0700

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,9 +1,8 @@
 import { createClient } from
 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
 
-const SUPABASE_URL = 'ใส่_URL_ของคุณ';
-
-const SUPABASE_ANON_KEY = 'ใส่_ANON_KEY_ของคุณ';
+const SUPABASE_URL = 'ใส่_SUPABASE_URL_ของคุณ';
+const SUPABASE_ANON_KEY = 'ใส่_SUPABASE_ANON_KEY_ของคุณ';
 
 export const supabase = createClient(
     SUPABASE_URL,
