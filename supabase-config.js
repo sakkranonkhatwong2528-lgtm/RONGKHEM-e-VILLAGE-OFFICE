794c4bd7cf7ce3bb9c758163bfe7873bb89bfd08
sakkranonkhatwong2528-lgtm <sakkranonkhatwong2528@gmail.com>
2026-08-21 17:12:43 +0700

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,28 +1,16 @@
 import { createClient } from
 "https://esm.sh/@supabase/supabase-js@2";
 
-// ========================================
-// SUPABASE CONFIG
-// ========================================
-
 const supabaseUrl =
 "https://qtnjtsigdgiwdsdfocmq.supabase.co";
 
 const supabaseAnonKey =
-"วาง_PUBLISHABLE_OR_ANON_KEY_ตรงนี้";
-
-// ========================================
-// CREATE CLIENT
-// ========================================
+"sb_publishable_วางคีย์เต็มของคุณตรงนี้";
 
 const supabaseClient =
 createClient(
   supabaseUrl,
   supabaseAnonKey
 );
-
-// ========================================
-// EXPORT
-// ========================================
 
 export default supabaseClient;
