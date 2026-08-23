c185cc3a869a0b322d1150b71925a3db820efa1d
sakkranonkhatwong2528-lgtm <sakkranonkhatwong2528@gmail.com>
2026-08-23 11:41:18 +0700

    Create supabase-config.js

---
+++ supabase-config.js
@@ -0,0 +1,9 @@
+const SUPABASE_URL = "https://qtnjtsigdgiwdsdfocmq.supabase.co";
+
+const SUPABASE_ANON_KEY =
+"sb_publishable_xsHzrDWMBlYwWyTIrojR4Q_8W6mEbJN";
+
+const supabaseClient = supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_ANON_KEY
+);
