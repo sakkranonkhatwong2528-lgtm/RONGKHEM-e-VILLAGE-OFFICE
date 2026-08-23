3dd3bfc670b4670a44ba210cbad1bcf848a1c907
sakkranonkhatwong2528-lgtm <sakkranonkhatwong2528@gmail.com>
2026-08-23 19:12:44 +0700

    Delete supabase-config.js

--- supabase-config.js
+++
@@ -1,9 +0,0 @@
-const SUPABASE_URL = "https://qtnjtsigdgiwdsdfocmq.supabase.co";
-
-const SUPABASE_ANON_KEY =
-"sb_publishable_xsHzrDWMBlYwWyTIrojR4Q_8W6mEbJN";
-
-const supabaseClient = supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_ANON_KEY
-);
